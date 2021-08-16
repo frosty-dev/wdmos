a891aa3893fbc5262bbd23488c0cdfdbc1bb97b7
baldenysh <46993894+baldenysh@users.noreply.github.com>
2021-08-16 18:52:36 +0300

    yadebil

---
+++ build.bat
@@ -0,0 +1,4 @@
+mkdir build
+cd build
+cmake ../byond-extools -A Win32 
+cmake --build . --config RelWithDebInfo
--- byond-extools/src/monstermos/Reaction.cpp
+++ byond-extools/src/monstermos/Reaction.cpp
@@ -8,6 +8,7 @@ Reaction::Reaction(Value v)
 {
     List min_reqs = v.get("min_requirements");
     if(min_reqs.at("TEMP").type == DataType::NUMBER) min_temp_req = min_reqs.at("TEMP");
+    if(min_reqs.at("MAX_TEMP").type == DataType::NUMBER) max_temp_req = min_reqs.at("MAX_TEMP");
     if(min_reqs.at("ENER").type == DataType::NUMBER) min_ener_req = min_reqs.at("ENER");
     for(unsigned int i=0;i < TOTAL_NUM_GASES;i++)
     {
@@ -32,7 +33,7 @@ Reaction::Reaction(Value v)
 
 bool Reaction::check_conditions(const GasMixture& air) const
 {
-    if(air.get_temperature() < min_temp_req || air.thermal_energy() < min_ener_req)
+    if(air.get_temperature() < min_temp_req || air.get_temperature() > max_temp_req || air.thermal_energy() < min_ener_req)
     {
         return false;
     }
